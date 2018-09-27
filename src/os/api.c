70d9a77cfb6f9445ee776728bb2d6f1869458ab5
Konrad Sikorski <znfgnu@gmail.com>
2018-09-27 18:58:31 +0200

    updated api a bit

--- src/os/api.c
+++ src/os/api.c
@@ -9,6 +9,8 @@
 #include "hw/led.h"
 #include "hw/btn.h"
 #include "hw/time.h"
+#include "gfx/gfx.h"
+#include "gfx/font.h"
 
 const void* api_fn_pointers[] = {
 		(void*) 0,			// 0x00
@@ -16,6 +18,11 @@ const void* api_fn_pointers[] = {
 		(void*) btn_get,	// 0x02
 		(void*) led_set_rgb,	// 0x03
 		(void*) time_delay,	// 0x04
+		(void*) gfx_clear,	// 0x05
+		(void*) gfx_update,	// 0x06
+		(void*) gfx_set_clear_color,	// 0x07
+		(void*) gfx_set_pixel,	// 0x08
+		(void*) font_print_string,	// 0x09
 };
 
 void api_init(void) {
